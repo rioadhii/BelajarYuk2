23884ef12a61dd1b18ba748bcc57e01e05719972
rioadhii <rio.adhi.pratama11@gmail.com>
2019-06-30 18:31:34 +0700

    User.java

--- app/src/main/java/com/esaunggul/doayuk/User.java
+++ app/src/main/java/com/esaunggul/doayuk/User.java
@@ -3,15 +3,17 @@ package com.esaunggul.doayuk;
 public class User {
 
     public String username;
+    public String fullName;
     public String email;
 
     public User() {
         // Default constructor required for calls to DataSnapshot.getValue(User.class)
     }
 
-    public User(String username, String email) {
+    public User(String username, String email, String fullName) {
         this.username = username;
         this.email = email;
+        this.fullName = fullName;
     }
 
 }
